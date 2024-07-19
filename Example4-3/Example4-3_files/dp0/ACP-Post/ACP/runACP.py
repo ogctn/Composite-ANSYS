d3ddd0adf77a3ebcd6168d61f49911c8e915a718
ogctn <oguzalpcetin@gmail.com>
2024-07-19 05:40:16 +0300

    [ADD]: files.

---
+++ Example4-3/Example4-3_files/dp0/ACP-Post/ACP/runACP.py
@@ -0,0 +1,4 @@
+﻿# -*- coding: utf-8 -*-
+import compolyx
+db = compolyx.DB()
+db.open( path=u'C:\\Users\\MonsteR\\Documents\\Ansys\\Example4-3\\Example4-3_files\\dp0\\ACP-Post\\ACP\\ACP-Post.acph5' )
